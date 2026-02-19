22a168c7d424eccf3897f7ea202727473afd81f3
Kevin Burke <kburke119@gmail.com>
2026-02-19 11:47:48 -0500

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,10 +5,11 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+  alias: {
+    '@': path.resolve(__dirname, './src'),
+    '@assets': path.resolve(__dirname, './src/assets'),
   },
+},
   build: {
     outDir: 'dist',
   },
